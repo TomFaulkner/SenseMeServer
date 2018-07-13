b4cb57d4b6d4c116a4835afc30dc5cd674decdd9
Tom Faulkner <tomfaulkner@gmail.com>
2018-07-12 23:35:06 -0500

    fix indices, readme

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # SenseMeServer
 Python REST API Server for SenseMe devices
+
+Not much to see here at the moment, but the idea is a Hug based REST API to access and control SenseMe devices using the [SenseMe library](https://github.com/TomFaulkner/SenseMe "SenseMe library").
+
+# Requirements
+- Python 3.6+
+- Hug (version tbd.)
+
+The SenseMe library requires Python 3.6 for fstrings. This project is relying on the ordered nature of dictionaries in 3.6.
+
+# Future
+Once this is ready for use I'll include a Dockerfile for ease of use.
--- sensemeserver/__main__.py
+++ sensemeserver/__main__.py
@@ -2,38 +2,34 @@
 from typing import Union
 
 import hug
-
 from senseme import SenseMe
 
 from load_devices import load_devices
 """
-config loader / saver
-
 /devicename/feature/setting/value
 
 feature - fan/light
 setting - brightness/power/whoosh/motion/etc
 """
 
-# load the config
-# for now though
-config = {}
-
-# load devices
 device_db = load_devices()
 
 # convert loaded devices list to senseme device object list
-devices = []
-for device in device_db:
-    devices.append(SenseMe(**device))
+devices = {}
+for idx, device in enumerate(device_db):
+    # devices.append(SenseMe(**device))
+    name = device['name'] if device['name'] else idx    
+    devices[name] = SenseMe(**device) 
 
-def _device(device_id: Union[str, int], devices: list):
-    if isinstance(device_id, str):
-        try:
-            return devices[int(device_id)]
-        except ValueError:
-            return devices[device_id]
-    return devices[devices.keys()[device_id]]
+
+def _device(device_id: str, devices: list) -> SenseMe:
+    """helper to support {device} being either index or name"""
+    try:
+        idx = int(device_id)
+        return devices[tuple(devices.keys())[idx]]
+    except ValueError:
+        return devices[device_id]
+
 
 @hug.get('/{device}/fan/speed')
 def fan_speed(device: str):
@@ -41,6 +37,7 @@ def fan_speed(device: str):
         return _device(device, devices).speed 
     except:
         return devices
+
 
 @hug.post('/{device}/fan/speed')
 def fan_speed(device: str, speed: int):
